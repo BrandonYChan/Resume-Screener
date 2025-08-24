7cdf214a84a104456953ac542b6f954363e394ef
BrandonYChan <bchany100@gmail.com>
2025-08-24 16:49:06 -0400

    cleanup files

--- screener_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('gradio/', views.gradio_embed, name='gradio_embed'), 
-    path('save-input/', views.save_gradio_input, name='save_gradio_input'), 
-]
